435d8facfd6eff058807b2b9cd23a9fa9e63e0cb
harsh796719 <96861098+harsh796719@users.noreply.github.com>
2025-02-22 01:48:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import ghPages from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +12,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    ghPages(),
   ].filter(Boolean),
   resolve: {
     alias: {
