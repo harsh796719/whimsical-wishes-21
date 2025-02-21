f4fa02335b311f0522f8dc5f7c329393267c03d9
harshh100 <harshpatel00024@gmail.com>
2025-02-22 02:10:04 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-import ghPages from "vite-plugin-gh-pages";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-    ghPages(),
-  ].filter(Boolean),
+  plugins: [react()].filter(Boolean),
+  base: "/whimsical-wishes-21",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
